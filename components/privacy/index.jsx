f920ef96caa525c07491403039377bcc3fcdbf64
Sung Kim <sungkim11@gmail.com>
2023-07-11 02:50:22 -0700

    changes to both privacy policy and cookie policy

--- components/privacy/index.jsx
+++ components/privacy/index.jsx
@@ -11,16 +11,58 @@ const Privacy = () => {
             <div className="text-lg font-medium text-primary mt-5 mb-40 whitespace-pre-wrap">
                 {
                     `
-Welcome to mediumworx! We’re here to help you enhance your daily operations by leveraging the power of artificial intelligence. These Terms of Service ("Terms") govern your access to and use of mediumworx LLC's services, including our website (mediumworx), applications, content, and other services provided by mediumworx LLC (collectively, the "Service"). By accessing or using the Service, you agree to be bound by these Terms.
+Welcome to mediumworx! We’re here to help you enhance your daily operations by leveraging the power of artificial intelligence. At Mediumworx LLC ("Mediumworx," "we," "us," or "our"), we are committed to protecting your privacy. This Privacy Policy outlines how we collect, use, disclose, and protect the personal information we receive from users of our consulting services and website (the "Services").
 
-1. License Grant
-Subject to these Terms, mediumworx LLC grants you a limited, non-exclusive, non-transferable, non-sublicensable license to access and use the Service for your internal business purposes.
-    
-2. Restrictions
-You may not: (a) reverse engineer, disassemble, or decompile the Service, or attempt to discover the source code or underlying ideas or algorithms of the Service; (b) modify, alter, or create derivative works based on the Service; (c) sublicense, rent, lease, or otherwise transfer the Service; (d) use the Service for any purpose that is unlawful or prohibited by these Terms; (e) use the Service to violate any applicable law or regulation; or (f) use the Service in any manner that could damage, disable, overburden, or impair the Service.
-    
-3. Intellectual Property
-All intellectual property rights, including copyrights, patents, trademarks, and trade secrets, in the Service are owned by mediumworx LLC or its licensors. You acknowledge that you do not acquire any ownership rights in the Service by using the Service.
+1. Information We Collect
+1.1 Personal Information:
+We may collect personal information that you provide to us when using our Services. This may include your name, email address, phone number, job title, company name, and any other information you choose to provide.
+
+1.2 Usage Information:
+We may collect non-personal information about how you use our Services. This includes information such as your IP address, browser type, operating system, referring website, pages visited, and the date and time of each visit. We may also collect information about your interactions with our emails, such as whether you opened or clicked on them.
+
+2. Use of Information
+2.1 Providing Services:
+We use the personal information we collect to deliver our consulting services to you, respond to your inquiries, and provide you with the information and support you request.
+
+2.2 Communication:
+We may use your personal information to communicate with you about our Services, including sending you updates, newsletters, and promotional materials. You can opt-out of receiving marketing communications at any time by following the unsubscribe instructions provided in the emails or contacting us directly.
+
+2.3 Analytics and Improvement:
+We may use the information we collect to analyze trends, administer our Services, monitor usage patterns, and improve our website and Services' functionality and performance.
+
+3. Disclosure of Information
+3.1 Service Providers:
+We may engage third-party service providers to assist us in delivering our Services or to perform business functions on our behalf, such as hosting, data analysis, and customer support. These service providers may have access to your personal information but are only authorized to use it as necessary to provide the services to us.
+
+3.2 Legal Compliance:
+We may disclose your personal information if required to do so by law or if we believe that such action is necessary to comply with applicable laws, regulations, legal processes, or government requests.
+
+3.3 Business Transfers:
+If Mediumworx is involved in a merger, acquisition, sale of assets, or other business transaction, your personal information may be transferred as part of that transaction. We will notify you via email and/or a prominent notice on our website of any such change in ownership or control of your personal information.
+
+4. Data Security
+We take reasonable measures to protect the personal information we collect from unauthorized access, disclosure, alteration, and destruction. However, please note that no data transmission or storage can be guaranteed to be 100% secure. If you have any concerns about the security of your personal information, please contact us using the information provided at the end of this Privacy Policy.
+
+5. Your Choices
+5.1 Access and Correction:
+If you would like to access, update, correct, or delete any personal information we hold about you, please contact us using the information provided below. We will make reasonable efforts to fulfill your request, subject to any legal or contractual obligations.
+
+5.2 Do Not Track:
+Our website does not respond to "Do Not Track" signals. However, you can usually modify your browser settings to decline cookies or to notify you when a cookie is being placed on your device.
+
+6. Children's Privacy
+Our Services are not directed to individuals under the age of 16. We do not knowingly collect personal information from children. If you believe that we have inadvertently collected personal information from a child, please contact us, and we will promptly delete the information.
+
+7. Changes to this Privacy Policy
+We may update this Privacy Policy from time to time. The updated version will be indicated by the "Last Updated" date at the top of this Policy. We encourage you to review this Privacy Policy periodically to stay informed of any changes.
+
+8. Contact Us
+If you have any questions, concerns, or requests regarding this Privacy Policy or our privacy practices, please contact us at:
+
+Mediumworx LLC
+info@mediumworx.com
+
+Please note that this Privacy Policy applies only to the Services provided by Mediumworx LLC and does not extend to any third-party websites or services linked to our website. We are not responsible for the privacy practices or content of such third parties.
                     `
                 }
 
