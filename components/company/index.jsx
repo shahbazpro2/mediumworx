d44d280db636ec1520c3346547ae277df0225833
Sung Kim <sungkim11@gmail.com>
2023-07-11 02:31:45 -0700

    Changed the Company page.

--- components/company/index.jsx
+++ components/company/index.jsx
@@ -7,7 +7,7 @@ const Company = () => {
             <div className="text-center md:w-[600px] m-auto">
                 <div className="text-4xl font-bold">Meet mediumworx</div>
             </div>
-            <div className="text-lg font-medium text-primary mt-5 mb-40">We are a team of data scientists, data engineers, machine learning engineers, and software engineers who are passionate about leveraging the power of artificial intelligence to we aim to enhance your daily operations. We are a distributed team with an office in Los Angeles, CA. <a href="mailto:mediumworx@example.com" className='underline'>Come join us!</a> </div>
+            <div className="text-lg font-medium text-primary mt-5 mb-40">We are a team of data scientists, data engineers, machine learning engineers, and software engineers who are passionate about leveraging the power of artificial intelligence to we aim to enhance your daily operations. We are a distributed team with an office in Los Angeles, CA. <a href="mailto:jobs@mediumworx.com" className='underline'>Come join us!</a> </div>
 
         </div>
     )
